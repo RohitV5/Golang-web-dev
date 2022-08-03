7fa0e75d7a2820fc86c0aeca9ab933b9e355940b
Rohit Verma <verma.rohit.in@gmail.com>
2022-08-03 18:16:57 +0530

    Feature : Go api http response writer and request

--- 24BuildApi/main.go
+++ 24BuildApi/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Model for course - file
 type Course struct {
 	CourseId    string  `json:"courseid"`
@@ -20,6 +26,20 @@ func IsEmpty(c *Course) bool {
 
 //fake DB
 var courses []Course
+
+//controllers - file
+
+//serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get Alll courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
 
 func main() {
 
