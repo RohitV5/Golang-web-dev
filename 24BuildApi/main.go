ef6f271979ed7be5bc71a0d22ffd7f03926c59ec
Rohit Verma <verma.rohit.in@gmail.com>
2022-08-04 21:45:04 +0530

    Feature : Get course by id

--- 24BuildApi/main.go
+++ 24BuildApi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Model for course - file
@@ -39,6 +41,24 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get Alll courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get One courses")
+	w.Header().Set("Content-Type", "application/json")
+
+	//grab id from request
+	params := mux.Vars(r)
+
+	//loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
 }
 
 func main() {
