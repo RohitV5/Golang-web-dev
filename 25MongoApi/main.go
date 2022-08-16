5ee7d3312fe4d2696313bdb8ecddc2e178518ff6
Rohit Verma <verma.rohit.in@gmail.com>
2022-08-16 12:34:00 +0530

    Feature : HTTP Routes

--- 25MongoApi/main.go
+++ 25MongoApi/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rohitv5/Golang-web-dev/25MongoApi/router"
+)
+
+func main() {
+	fmt.Println("MongoDB API")
+	r := router.Router()
+	fmt.Println("Server getting started")
+	http.ListenAndServe(":4000",r)
+	fmt.Println("Listening at port 4000")
+}
